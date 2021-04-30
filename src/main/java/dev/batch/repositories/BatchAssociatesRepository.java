d03d96572ae087b2ad30e63591f14d0dacb47eb2
Matthew Carlson <cmatt97@vt.edu>
2021-04-30 14:56:16 -0400

    get/add associates to batch

---
+++ src/main/java/dev/batch/repositories/BatchAssociatesRepository.java
@@ -0,0 +1,10 @@
+package dev.batch.repositories;
+
+import dev.batch.models.BatchAssociates;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BatchAssociatesRepository extends JpaRepository<BatchAssociates, BatchAssociates.BatchAssociatesId> {
+	List<BatchAssociates> findAllInBatch(long batchId);
+}
