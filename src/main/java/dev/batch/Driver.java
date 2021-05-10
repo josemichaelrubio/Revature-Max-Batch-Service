fc6c7221bd6bf04ea1fac042689e75065ccd2d2b
Karl Kanitsch <karl.kanitsch@revature.net>
2021-05-10 19:14:53 -0400

    Adding @EnableDiscoveryClient

--- src/main/java/dev/batch/Driver.java
+++ src/main/java/dev/batch/Driver.java
@@ -3,11 +3,13 @@ package dev.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Driver {
 
     public static void main(String[] args) {
