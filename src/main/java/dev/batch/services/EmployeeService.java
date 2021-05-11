e2b1ebd7bacdaebb486a93ed54f26ecfb1acb3cc
Matthew Carlson <cmatt97@vt.edu>
2021-05-11 16:13:41 -0400

    good coverage for employeeService

--- src/main/java/dev/batch/services/EmployeeService.java
+++ src/main/java/dev/batch/services/EmployeeService.java
@@ -41,20 +41,20 @@ public class EmployeeService {
 		UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(EMPLOYEE_SERVICE_URL);
 		StringBuilder stringOfIds = new StringBuilder();
 		employeeIds.forEach(id-> stringOfIds.append(",").append(id));
-
-		//Todo is this still necessary?
-//		stringOfIds.deleteCharAt(0); // probably a better way to do this
+		stringOfIds.deleteCharAt(0);
 
 		// Don't remember the name of the query param
 		uriComponentsBuilder.queryParam("id", stringOfIds);
 		String uri = uriComponentsBuilder.toUriString();
-		uri += "&field=";
-		if (includeQuizScores)
-			uri += "quiz-scores,";
-		if (includeTopicCompetencies)
-			uri += "topic-competencies,";
-		if (includeQCFeedback)
-			uri += "qc-feedbacks";
+		if (includeQCFeedback || includeQuizScores || includeTopicCompetencies) {
+			uri += "&field=";
+			if (includeQuizScores)
+				uri += "quiz-scores,";
+			if (includeTopicCompetencies)
+				uri += "topic-competencies,";
+			if (includeQCFeedback)
+				uri += "qc-feedbacks";
+		}
 
 
 		ResponseEntity<EmployeeDTO[]> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<EmployeeDTO[]>(new HttpHeaders()), EmployeeDTO[].class);
