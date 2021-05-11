dd63e32f86862f3c3967344cfccb530708880b17
Karl Kanitsch <karl.kanitsch@revature.net>
2021-05-11 11:33:44 -0400

    adding ports to ip address

--- src/main/java/dev/batch/services/CurriculumService.java
+++ src/main/java/dev/batch/services/CurriculumService.java
@@ -24,7 +24,7 @@ public class CurriculumService {
 
 
 	// use service name in discovery service
-	private static final String CURRICULUM_SERVICE_URL = "http://40.122.154.60/curriculum";
+	private static final String CURRICULUM_SERVICE_URL = "http://40.122.154.60:8085/curriculum";
 //	private static final String CURRICULUM_SERVICE_URL = "http://curriculum-service/curriculum";
 
 
--- src/main/java/dev/batch/services/EmployeeService.java
+++ src/main/java/dev/batch/services/EmployeeService.java
@@ -23,7 +23,7 @@ public class EmployeeService {
 	RestTemplate restTemplate;
 
 	// use service name in discovery service
-	private static final String EMPLOYEE_SERVICE_URL = "http://20.185.67.43/employees";
+	private static final String EMPLOYEE_SERVICE_URL = "http://20.185.67.43:8082/employees";
 //	private static final String EMPLOYEE_SERVICE_URL = "http://employee-service/employees";
 
 	public Employee getEmployeeById(long id){
